98322dd8d7256b302eaabf56dbb511c15e6c4143
Timothee Poisot <tim@poisotlab.io>
2015-07-15 13:23:43 +0000

    version 2.0.2

---
+++ R/metaweb.r
@@ -0,0 +1,16 @@
+#' @title Returns a metaweb given a list of networks
+#' @description
+#' Given a list of networks, this function returns the metaweb
+#'
+#' @param n a \code{list} of graphs
+#' @export
+metaweb <- function(n){
+   if(length(n)==1)
+   {
+      warning("There is a single network in the data you passed")
+      return(n)
+   }
+   M <- n[[1]]
+   for(i in c(2:length(n))) M <- M + n[[i]]
+   return(M)
+}
